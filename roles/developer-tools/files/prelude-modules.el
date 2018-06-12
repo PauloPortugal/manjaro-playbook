117515b63e20b532ae623ca6713daad68f69caa6
pmonteiro <paulo.from.portugal@gmail.com>
2018-06-12 23:59:01 +0100

    Install and configure emacs prelude

---
+++ roles/developer-tools/files/prelude-modules.el
@@ -0,0 +1,21 @@
+;;; Tools support
+(require 'prelude-erc) ;; Emacs IRC client
+(require 'prelude-ivy) ;; A mighty modern alternative to ido
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company)
+
+;;; Programming languages support
+(require 'prelude-c)
+(require 'prelude-clojure)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+(require 'prelude-js)
+(require 'prelude-lisp)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-perl)
+(require 'prelude-scheme)
+(require 'prelude-shell)
+(require 'prelude-scss)
+(require 'prelude-xml)
+(require 'prelude-yaml)
